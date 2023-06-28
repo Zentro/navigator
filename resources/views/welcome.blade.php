ffec4a1514255f0fa58c9eb04c629128f6aef1d1
Rafael <rbgalvan@cougarnet.uh.edu>
2023-06-27 22:03:14 -0500

    Added navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,16 +48,55 @@
 
             <div class="flex items-center space-x-6">
                 @if (Route::has('login'))
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm font-medium text-white hover:text-gray-100">Welcome back, {{ Auth::user()->name }}</a>
-                    @else
-                        <a href="{{ url('login') }}" class="text-sm font-medium text-white hover:text-gray-100">Sign in</a>
-                        <a href="{{ url('register') }}" class="text-sm font-medium text-white hover:text-gray-100">Create an account</a>
-                    @endauth
+                @auth
+                <a href="{{ url('/dashboard') }}" class="text-sm font-medium text-white hover:text-gray-100">Welcome back, {{ Auth::user()->name }}</a>
+                @else
+                <a href="{{ url('login') }}" class="text-sm font-medium text-white hover:text-gray-100">Sign in</a>
+                <a href="{{ url('register') }}" class="text-sm font-medium text-white hover:text-gray-100">Create an account</a>
+                @endauth
                 @endif
             </div>
         </div>
     </div>
+    <nav class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 justify-between">
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="flex flex-shrink-0 items-center">
+                        <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                        <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                    </div>
+                    <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+                        <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
+                        <a href="#" class="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900">Dashboard</a>
+                        <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Team</a>
+                        <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Projects</a>
+                        <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Calendar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hamburger -->
+        <div class="-mr-2 flex items-center sm:hidden">
+            <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100  focus:text-gray-500 transition duration-150 ease-in-out">
+                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div :class="{'block': open, 'hidden': ! open}" class="sm:hidden hidden">
+            <div class="space-y-1 pt-2 pb-4">
+                <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
+                <a href="#" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Dashboard</a>
+                <a href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Team</a>
+                <a href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Projects</a>
+                <a href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Calendar</a>
+            </div>
+        </div>
+    </nav>
 </body>
 
 </html>
