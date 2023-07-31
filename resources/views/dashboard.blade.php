c85c47e7121c1e77de44d59867e4c235263b129c
Rafael <rbgalvan@cougarnet.uh.edu>
2023-07-31 03:07:25 -0500

    Minor UI adjustments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex justify-center">
                 <div class="p-6 text-gray-900 dark:text-gray-100 font-bold text-2xl">
-                    {{ __("Welcome to Energy Rate Navigator!") }}
+                    {{ __("Welcome to EnergyRate Navigator!") }}
                 </div>
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 mt-4 flex justify-center space-x-10">
